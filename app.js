98a47212a149cd73f5049394a26f7a0355c648aa
Adhikansh Mittal <42075023+HrithikMittal@users.noreply.github.com>
2019-01-15 22:11:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,93 +3,85 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var Details = require('./app/models/details');
+var MongoClient = require('mongodb').MongoClient;
 
 // Configure app for bodyParser()
 // lets us grab data from the body of POST
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
 
 // Set up port for server to listen on
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3001;
 
 // Connect to DB
-mongoose.connect('mongodb+srv://hrithik:hrithik123@users-6oe1e.gcp.mongodb.net/test?retryWrites=true'); //Will be different for the localhost and the different user.
+var url = 'mongodb+srv://hrithik:hrithik123@users-6oe1e.gcp.mongodb.net/test?retryWrites=true';
+MongoClient.connect(url, (err, db) => {
+  const dbo = db.db("test");
 
-//API Routes
-var router = express.Router();
+  //API Routes
+  var router = express.Router();
 
-// Routes will all be prefixed with /API
-app.use('/api',router);
+  // Routes will all be prefixed with /API
+  app.use('/api', router);
 
-//MIDDLE WARE-
-router.use(function(req,res,next){
-  console.log('FYI...There is some processing currently going down');
-  next();
-});
+  //MIDDLE WARE-
+  router.use(function (req, res, next) {
+    console.log('FYI...There is some processing currently going down');
+    next();
+  });
 
-// test route
-router.get('/',function(req,res){
-  res.json({message:'Welcome !'});
-});
-
-router.route('/details')
-.post(function(req,res){
-  var person = new Details();
-  person.bname = req.body.bname;
-  person.pno = req.body.pno;
-  person.email = req.body.email;
-  person.baddress = req.body.baddress;
-
-  person.save(function(err){
-    if(err){
-       res.send(err);
-    }
-    res.json({message:'User was successfully Signup'});
-});
-})
-
-  .get(function(req,res){
-    Details.find(function(err,details){
-      if(err){
-        res.send(err);
-      }
-      res.json(details);
+  // test route
+  router.get('/', function (req, res) {
+    res.json({
+      message: 'Welcome !'
     });
   });
 
-router.route('/details/:detail_id')
-   .get(function(req,res){
-     Details.findById(req.params.detail_id,function(err,detail){
-       if(err){
-         res.send(err);
-       }
-       res.json(detail);
-     });
-   });
+  router.route('/details')
+    .post(function (req, res) {
+      let status = true;
+      var person = new Details();
+      person.bname = req.body.bname;
+      person.pno = req.body.pno;
+      person.email = req.body.email;
+      person.baddress = req.body.baddress;
+      person.btype = req.body.btype;
+      person.fyear = req.body.fyear;
+      person.booksdate = req.body.booksdate;
 
-   router.route('/details/bname/:bname')
-      .get(function(req,res){
-        Details.find({bname:req.params.bname},function(err,detail){
-          if(err){
-            res.send(err);
+      dbo.collection("details").find({}).toArray(function (err, result) {
+        if (err) throw err;
+        for (var i = 0; i < result.length; i++) {
+          if (person.pno == result[i].pno) {
+            status = false;
           }
-          res.json(detail);
-        });
+        }
+        if (status == true) {
+          dbo.collection("details").insertOne(person, function (err, rese) {
+            if (err) throw {
+              message: "Error comes " + err.message,
+            };
+            console.log("1 document inserted sucessfully");
+            // res.send do not work here 
+          });
+        }
+        res.send("Proccess successfully done...");
+        return status;
       });
-    router.route('/details/pno/:pno')
-       .get(function(req,res){
-         Details.find({pno:req.params.pno},function(err,detail){
-           if(err){
-             res.send(err);
-           }
-           res.json(detail);
-         });
-       });
-
-
-
+    })
+    .get(function (req, res) {
+      Details.find(function (err, details) {
+        if (err) {
+          res.send(err);
+        }
+        res.json(details);
+      });
+    });
+});
 // Fire up server
 app.listen(port);
 
 // print friendly message to console
-console.log('Server listening on port '+port);
+console.log('Server listening on port ' + port);
